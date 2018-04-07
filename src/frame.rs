d319df51c8ea61acc91a79cd8eaec16c8b9e9977
Jamie Blondin <jblondin@gmail.com>
2018-04-07 11:22:01 -0400

    Added frame documentation

--- src/frame.rs
+++ src/frame.rs
@@ -1,3 +1,7 @@
+/*!
+Structs and implementation for Frame-level data structure. A `DataFrame` is a reference to an
+underlying data store, along with record-based filtering and sorting details.
+*/
 use std::rc::Rc;
 
 use bit_vec::BitVec;
@@ -6,17 +10,20 @@ use store::DataStore;
 use masked::FieldData;
 use field::{FieldIdent, FieldType};
 
+/// A data record filter.
 #[derive(Debug, Clone)]
 pub struct Filter {
     mask: BitVec,
     len: usize,
 }
 impl Filter {
+    /// Returns the length of this filter (the number of elements that pass the filter)
     pub fn len(&self) -> usize {
         self.len
     }
 }
 
+/// A data frame. A `DataStore` reference along with record-based filtering and sorting details.
 #[derive(Debug, Clone)]
 pub struct DataFrame {
     filter: Option<Filter>,
@@ -24,6 +31,7 @@ pub struct DataFrame {
     store: Rc<DataStore>,
 }
 impl DataFrame {
+    /// Number of rows that pass the filter in this frame.
     pub fn nrows(&self) -> usize {
         match self.filter {
             Some(ref filter) => filter.len(),
@@ -37,10 +45,11 @@ impl DataFrame {
     pub(crate) fn store_ref_count(&self) -> usize {
         Rc::strong_count(&self.store)
     }
+    /// Get the field type of a particular field in the underlying `DataStore`.
     pub fn get_field_type(&self, ident: &FieldIdent) -> Option<FieldType> {
         self.store.get_field_type(ident)
     }
-    pub fn has_same_store(&self, other: &DataFrame) -> bool {
+    pub(crate) fn has_same_store(&self, other: &DataFrame) -> bool {
         Rc::ptr_eq(&self.store, &other.store)
     }
 }
