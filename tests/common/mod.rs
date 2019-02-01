81d7b298fca7f8a9a6cedb84cafcdf672517eca3
Jamie Blondin <jblondin@gmail.com>
2019-01-31 19:01:17 -0500

    Re-enabled CSV integration test

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,23 +1,24 @@
-// use std::path::Path;
+use std::path::Path;
 
-// use csv_sniffer::metadata::Metadata;
+use csv_sniffer::metadata::Metadata;
 
-// // use agnes::source::csv2::{CsvSource, CsvReader, IntoCsvSrcSpec};
-// use agnes::source::{CsvSource, CsvReader};
-// use agnes::fieldlist::{AssocFields, FieldSpecs};
-// use agnes::data_types::csv::*;
+use agnes::source::csv::{CsvReader, CsvSource, IntoCsvSrcSpec};
 
-// pub fn load_csv_file<Spec>(filename: &str, spec: Spec) -> (CsvReader, Metadata)
-//     where Spec: FieldSpecs<Types> + AssocFields
-// // pub fn load_csv_file<Spec>(filename: &str, spec: Spec) -> (CsvReader<Spec::CsvSrcSpec>, Metadata)
-// //     where Spec: IntoCsvSrcSpec
-// {
-//     let data_filepath = Path::new(file!()) // start as this file
-//         .parent().unwrap()                 // navigate up to common directory
-//         .parent().unwrap()                 // navigate up to tests directory
-//         .join("data")                      // navigate into data directory
-//         .join(filename);                   // navigate to target file
+pub fn load_csv_file<Spec>(filename: &str, spec: Spec) -> (CsvReader<Spec::CsvSrcSpec>, Metadata)
+where
+    Spec: IntoCsvSrcSpec,
+{
+    let data_filepath = Path::new(file!()) // start as this file
+        .parent()
+        .unwrap() // navigate up to common directory
+        .parent()
+        .unwrap() // navigate up to tests directory
+        .join("data") // navigate into data directory
+        .join(filename); // navigate to target file
 
-//     let source = CsvSource::new(data_filepath.into()).unwrap();
-//     (CsvReader::new(&source, spec).unwrap(), source.metadata().clone())
-// }
+    let source = CsvSource::new(data_filepath.into()).unwrap();
+    (
+        CsvReader::new(&source, spec).unwrap(),
+        source.metadata().clone(),
+    )
+}
--- tests/csv.rs
+++ tests/csv.rs
@@ -1,55 +1,64 @@
-// #[macro_use] extern crate agnes;
-// extern crate csv_sniffer;
-// extern crate typenum;
+#[macro_use]
+extern crate agnes;
+extern crate csv_sniffer;
+extern crate typenum;
 
-// use agnes::data_types::csv::*;
+use agnes::cons::Nil;
+use agnes::label::LCons;
 
-// mod common;
+mod common;
 
-// #[test]
-// fn csv_load_test() {
-//     spec![
-//         let spec = {
-//             CountryName("Country Name"): String,
-//             CountryCode("Country Code"): String,
-//             Year1983("1983"): f64,
-//         };
-//     ];
-//     let (mut csv_rdr, metadata) = common::load_csv_file(
-//         "gdp.nopreamble.csv",
-//         spec
-//     );
-//     assert_eq!(metadata.num_fields, 63);
-//     assert_eq!(metadata.dialect.header.num_preamble_rows, 0);
-//     let dv: DataView = csv_rdr.read().unwrap().into();
-//     assert_eq!(dv.nrows(), 264);
-//     assert_eq!(dv.nfields(), 3);
-//     let subdv = dv.v(["Country Name", "1983"]);
-//     assert_eq!(subdv.nrows(), 264);
-//     assert_eq!(subdv.nfields(), 2);
-//     // println!("{}", dv.v(["Country Name", "1983"]));
-//     // println!("{}", dv.v(["Country Name", "1983"]).v(["Country Name"]));
-// }
+namespace![
+    pub namespace gdp {
+        field CountryName: String;
+        field CountryCode: String;
+        field Year1983: f64;
+    }
+];
 
-// #[test]
-// fn csv_load_test_skip() {
-//     spec![
-//         let spec = {
-//             CountryName("Country Name"): String,
-//             CountryCode("Country Code"): String,
-//             Year1983("1983"): f64,
-//         };
-//     ];    let (mut csv_rdr, metadata) = common::load_csv_file(
-//         "gdp.csv",
-//         spec
-//     );
-//     assert_eq!(metadata.num_fields, 63);
-//     assert_eq!(metadata.dialect.header.num_preamble_rows, 4);
-//     let dv: DataView = csv_rdr.read().unwrap().into();
-//     assert_eq!(dv.nrows(), 264);
-//     assert_eq!(dv.nfields(), 3);
-//     let subdv = dv.v(["Country Name", "1983"]);
-//     assert_eq!(subdv.nrows(), 264);
-//     assert_eq!(subdv.nfields(), 2);
-//     // println!("{}", dv.v(["Country Name", "1983"]));
-// }
+#[test]
+fn csv_load_test() {
+    use gdp::*;
+
+    let gdp_spec = spec![
+        fieldname gdp::CountryName = "Country Name";
+        fieldname gdp::CountryCode = "Country Code";
+        fieldname gdp::Year1983 = "1983";
+    ];
+
+    let (mut csv_rdr, metadata) = common::load_csv_file("gdp.nopreamble.csv", gdp_spec);
+
+    assert_eq!(metadata.num_fields, 63);
+    assert_eq!(metadata.dialect.header.num_preamble_rows, 0);
+    let dv = csv_rdr.read().unwrap().into_view();
+    assert_eq!(dv.nrows(), 264);
+    assert_eq!(dv.nfields(), 3);
+    let subdv = dv.v::<Labels![CountryName, Year1983]>();
+    assert_eq!(subdv.nrows(), 264);
+    assert_eq!(subdv.nfields(), 2);
+    println!("{}", subdv);
+}
+
+#[test]
+fn csv_load_test_skip() {
+    use gdp::*;
+
+    let gdp_spec = spec![
+        fieldname gdp::CountryName = "Country Name";
+        fieldname gdp::CountryCode = "Country Code";
+        fieldname gdp::Year1983 = "1983";
+    ];
+
+    let (mut csv_rdr, metadata) = common::load_csv_file("gdp.csv", gdp_spec);
+
+    assert_eq!(metadata.num_fields, 63);
+    assert_eq!(metadata.dialect.header.num_preamble_rows, 4);
+    let dv = csv_rdr.read().unwrap().into_view();
+    assert_eq!(dv.nrows(), 264);
+    assert_eq!(dv.nfields(), 3);
+
+    let subdv = dv.v::<Labels![CountryName, Year1983]>();
+    assert_eq!(subdv.nrows(), 264);
+    assert_eq!(subdv.nfields(), 2);
+    println!("{}", subdv);
+}
