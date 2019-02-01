9cc122d781b429755ac37b10546880f7d5d465dc
Jamie Blondin <jblondin@gmail.com>
2019-01-31 19:01:02 -0500

    Cleaned up commented code in CSV source parser

--- src/source/csv.rs
+++ src/source/csv.rs
@@ -4,8 +4,6 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use std::str::FromStr;
 
-// use typenum::{Unsigned, Add1, B1};
-
 use csv_sniffer::metadata::Metadata;
 use csv_sniffer::Sniffer;
 
@@ -47,26 +45,6 @@ impl CsvSource {
         &self.metadata
     }
 }
-
-// pub struct CsvSrcSpec<Field, FIdx, DType> {
-//     _field: PhantomData<Field>,
-//     _fidx: PhantomData<FIdx>,
-//     _dtype: PhantomData<DType>,
-//     src_idx: usize,
-// }
-
-// impl<Field, FIdx, DType> CsvSrcSpec<Field, FIdx, DType> {
-//     fn from_src_field_spec(src_idx: usize)
-//         -> Self
-//     {
-//         CsvSrcSpec {
-//             _field: PhantomData,
-//             _fidx: PhantomData,
-//             _dtype: PhantomData,
-//             src_idx,
-//         }
-//     }
-// }
 
 pub type CsvSrcSpecCons<Label, DType, Tail> = FieldPayloadCons<Label, DType, usize, Tail>;
 
@@ -123,99 +101,6 @@ where
     }
 }
 
-// pub trait FromSpec<Spec> {
-//     fn from_spec(spec: Spec, headers: &HashMap<String, usize>, num_fields: usize)
-//         -> Result<Self>
-//         where Self: Sized;
-// }
-
-// impl<Spec> FromSpec<Spec> for Nil {
-//     fn from_spec(_spec: Spec, _headers: &HashMap<String, usize>, _num_fields: usize)
-//         -> Result<Nil>
-//     {
-//         Ok(Nil)
-//     }
-// }
-
-// impl<Field, SrcFIdx, CsvSrcSpecTail, SpecTail> FromSpec<SpecCons<Field, SrcFIdx, SpecTail>>
-//     for CsvSrcSpecCons<Field, CsvSrcSpecTail>
-//     where SrcFIdx: Position,
-//           CsvSrcSpecTail: FromSpec<SpecTail>
-// {
-//     fn from_spec(
-//         spec: SpecCons<Field, SrcFIdx, SpecTail>,
-//         headers: &HashMap<String, usize>,
-//         num_fields: usize
-//     )
-//         -> Result<CsvSrcSpecCons<Field, CsvSrcSpecTail>>
-//     {
-//         let idx = match spec.head.payload {
-//             FieldDesignator::Expr(ref s) => *headers.get(s)
-//                 .ok_or(AgnesError::FieldNotFound(FieldIdent::Name(s.to_string())))?,
-//             FieldDesignator::Idx(_) => {
-//                 let idx = SrcFIdx::POS;
-//                 if idx >= num_fields {
-//                     return Err(AgnesError::IndexError { index: idx, len: num_fields });
-//                 };
-//                 idx
-//             }
-//         };
-//         Ok(Cons {
-//             head: idx.into(),
-//             // head: CsvSrcSpec::from_src_field_spec(idx),
-//             tail: FromSpec::from_spec(spec.tail, headers, num_fields)?
-//         })
-//     }
-// }
-
-// pub trait AttachSrcPos {
-//     type WithSrcPos;
-//     fn attach(self, headers: &HashMap<String, usize>, num_fields: usize)
-//         -> Result<Self::WithSrcPos>;
-// }
-// impl AttachSrcPos for Nil {
-//     type WithSrcPos = Nil;
-//     fn attach(self, _headers: &HashMap<String, usize>, _num_fields: usize)
-//         -> Result<Nil>
-//     {
-//         Ok(Nil)
-//     }
-// }
-// impl<Field, FIdx, SrcFIdx, DType, Tail> AttachSrcPos
-//     for SpecCons<Field, FIdx, SrcFIdx, DType, Tail>
-//     where SrcFIdx: Unsigned,
-//           Tail: AttachSrcPos
-// {
-//     type WithSrcPos = Cons<CsvSrcSpec<Field, FIdx, DType>, Tail::WithSrcPos>;
-//     fn attach(self, headers: &HashMap<String, usize>, num_fields: usize)
-//         -> Result<Self::WithSrcPos>
-//     {
-//         let idx = match self.head.src_name {
-//             FieldDesignator::Expr(ref s) => *headers.get(s)
-//                 .ok_or(AgnesError::FieldNotFound(FieldIdent::Name(s.to_string())))?,
-//             FieldDesignator::Idx(_) => {
-//                 let idx = SrcFIdx::to_usize();
-//                 if idx >= num_fields {
-//                     return Err(AgnesError::IndexError { index: idx, len: num_fields });
-//                 };
-//                 idx
-//             }
-//         };
-//         Ok(Cons {
-//             head: CsvSrcSpec::from_src_field_spec(idx),
-//             tail: self.tail.attach(headers, num_fields)?
-//         })
-//     }
-
-// }
-
-// impl<Field, FIdx, DType, Payload, Tail> AssocFields
-//     for Cons<CsvSrcSpec<Field, FIdx, DType>, Tail>
-//     where Tail: AssocFields,
-// {
-//     type Fields = FieldCons<Field, FIdx, DType, Payload, Tail::Fields>;
-// }
-
 pub trait BuildDStore {
     type OutputFields: AssocStorage;
     fn build(&mut self, src: &CsvSource) -> Result<DataStore<Self::OutputFields>>;
@@ -227,18 +112,12 @@ impl BuildDStore for Nil {
     }
 }
 impl<Label, DType, Tail> BuildDStore for FieldPayloadCons<Label, DType, usize, Tail>
-// for Cons<
-//     CsvSrcSpec<Field, Add1<<Tail::OutputFields as FieldIndex>::FIdx>, DType>,
-//     Tail
-// >
 where
     Tail: BuildDStore,
     DataStore<<Tail as BuildDStore>::OutputFields>: AddFieldFromValueIter<Label, DType>,
     Tail::OutputFields: PushBack<FieldSpec<Label, DType>>,
     <Tail::OutputFields as PushBack<FieldSpec<Label, DType>>>::Output: AssocStorage,
     Label: Debug,
-    //       Tail::OutputFields: FieldIndex,
-    //       <Tail::OutputFields as FieldIndex>::FIdx: Add<B1>,
     DType: FromStr + Debug + Default + Clone,
     ParseError: From<<DType as FromStr>::Err>,
 {
@@ -281,37 +160,19 @@ where
     }
 }
 
-/// Reader object responsible for converting a CSV file into a data store.
-// #[derive(Debug)]
-// pub struct CsvReader<Spec: AttachSrcPos> {
-//     src: CsvSource,
-//     csv_src_spec: Spec::WithSrcPos,
-// }
-
 #[derive(Debug)]
 pub struct CsvReader<CsvSpec> {
     src: CsvSource,
     csv_src_spec: CsvSpec,
 }
 
-impl<CsvSrcSpec> CsvReader<CsvSrcSpec>
-// where Spec: AttachSrcPos + AssocFields
-{
+impl<CsvSrcSpec> CsvReader<CsvSrcSpec> {
     /// Create a new CSV reader from a CSV source specification. This will process header row (if
     /// exists), and verify the fields specified in the `CsvSource` object exist in this CSV file.
-    // pub fn new<SrcFIdx, SpecTail>(src: &CsvSource, spec: SpecCons<Field, SrcFIdx, SpecTail>)
     pub fn new<Spec>(src: &CsvSource, spec: Spec) -> Result<CsvReader<Spec::CsvSrcSpec>>
     where
-        Spec: IntoCsvSrcSpec<CsvSrcSpec = CsvSrcSpec>, // SrcFIdx: Position,
-                                                       // Spec: AssocField<Field=Field>,
-                                                       // SpecCons<Field, SrcFIdx, SpecTail>:
-                                                       //   IntoCsvSrcSpec<CsvSrcSpec=CsvSrcSpecCons<Field, Tail>>,
-                                                       // CsvSrcSpecCons<Field, Tail>: FromSpec<SpecCons<Field, SrcFIdx, SpecTail>>
-                                                       // CsvSrcSpecCons<Field, Tail>: FromSpec<SpecCons<Field, SrcFIdx, SpecTail>>
-                                                       // Tail: FromSpec<SpecTail>
-                                                       // where Spec: Debug
+        Spec: IntoCsvSrcSpec<CsvSrcSpec = CsvSrcSpec>,
     {
-        // println!("spec: {:?}", spec);
         let file_reader = LocalFileReader::new(&src.src)?;
         let mut csv_reader = src.metadata.dialect.open_reader(file_reader)?;
 
@@ -344,16 +205,7 @@ impl<CsvSrcSpec> CsvReader<CsvSrcSpec>
     /// Read a `CsvSource` into a `DataStore` object.
     pub fn read(&mut self) -> Result<DataStore<CsvSrcSpec::OutputFields>>
     where
-        // Cons<Field, Tail>: DataStorage,
         CsvSrcSpec: BuildDStore,
-        // FieldPayloadCons<Field, usize, Tail>: BuildDStore<OutputFields=Cons<Field, Tail>>
-        // -> Result<DataStore<StorageCons<Field, Tail>>>
-        // where Field: FieldTypes
-        // pub fn read(&mut self) -> Result<DataStore<<Spec::WithSrcPos as AssocFields>::Fields>>
-        // where Spec::Fields: DataStorage,
-        //       Spec::WithSrcPos: AssocFields
-        //         + BuildDStore<OutputFields=<Spec::WithSrcPos as AssocFields>::Fields>,
-        //       <Spec::WithSrcPos as AssocFields>::Fields: DataStorage,
     {
         self.csv_src_spec.build(&self.src)
     }
