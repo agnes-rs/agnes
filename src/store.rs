b57ceeb4235e2046281d5e56ba20a6011987c276
Jamie Blondin <jblondin@gmail.com>
2019-02-09 15:26:17 -0500

    Documented datastore

--- src/store.rs
+++ src/store.rs
@@ -1,3 +1,7 @@
+/*!
+Data storage struct and implementation. [DataStore](struct.DataStore.html) represents and stores the
+data from a single data source.
+*/
 use std::fmt::Debug;
 use std::ops::Deref;
 use std::rc::Rc;
@@ -16,6 +20,7 @@ use label::*;
 use select::{FieldSelect, SelectFieldByLabel};
 use view::{DataView, FrameLookupCons, ViewFrameCons};
 
+/// Local `Rc` wrapper type for [FieldData](../field/struct.FieldData.html) objects.
 #[derive(Debug, Hash, PartialEq, Eq)]
 pub struct DataRef<DType>(pub Rc<FieldData<DType>>);
 
@@ -73,14 +78,21 @@ where
     }
 }
 
+/// Type alias for main data store cons-list. Each `head` contains label and data type information
+/// along with a [DataRef(struct.DataRef.html)] reference to the data for this field.
 pub type StorageCons<Label, DType, Tail> = FieldPayloadCons<Label, DType, DataRef<DType>, Tail>;
 
+/// Primary `agnes` data storage object. `Fields` is a [FieldCons](../fieldlist/type.FieldCons.html)
+/// cons-list which implements [AssocStorage](trait.AssocStorage.html); the `DataStore` contains
+/// this associated storage structure.
 #[derive(Debug)]
 pub struct DataStore<Fields: AssocStorage> {
     data: Fields::Storage,
 }
 
+/// Provide an associated [StorageCons](type.StorageCons.html) cons-list with `Self`.
 pub trait AssocStorage {
+    /// Associated [StorageCons](type.StorageCons.html) cons-list.
     type Storage: Debug;
 }
 impl<Label, DType, Tail> AssocStorage for FieldCons<Label, DType, Tail>
@@ -105,7 +117,9 @@ where
     }
 }
 
+/// Trait to provide the number of rows of this data structure.
 pub trait NRows {
+    /// Return the number of rows in this data structure.
     fn nrows(&self) -> usize;
 }
 impl NRows for Nil {
@@ -129,25 +143,43 @@ where
     }
 }
 
+/// Type alias for a reference to a [FieldData](../field/struct.FieldData.html) along with label
+/// and data type annotation.
 pub type NewFieldStorage<NewLabel, NewDType> =
     Labeled<NewLabel, TypedValue<NewDType, DataRef<NewDType>>>;
 
 macro_rules! make_add_field {
     (
-        $add_trait:tt $add_fn:tt;
-        $add_valiter_trait:tt $add_valiter_fn:tt;
-        $add_iter_trait:tt $add_iter_fn:tt;
-        $add_cloned_valiter_trait:tt $add_cloned_valiter_fn:tt;
-        $add_cloned_iter_trait:tt $add_cloned_iter_fn:tt;
-        $add_empty_trait:tt $add_empty_fn:tt;
+        $(#[$add_trait_doc:meta])* trait $add_trait:tt;
+        $(#[$add_fn_doc:meta])* fn $add_fn:tt;
+
+        $(#[$add_valiter_trait_doc:meta])* trait $add_valiter_trait:tt;
+        $(#[$add_valiter_fn_doc:meta])* fn $add_valiter_fn:tt;
+
+        $(#[$add_iter_trait_doc:meta])* trait $add_iter_trait:tt;
+        $(#[$add_iter_fn_doc:meta])* fn $add_iter_fn:tt;
+
+        $(#[$add_cloned_valiter_trait_doc:meta])* trait $add_cloned_valiter_trait:tt;
+        $(#[$add_cloned_valiter_fn_doc:meta])* fn $add_cloned_valiter_fn:tt;
+
+        $(#[$add_cloned_iter_trait_doc:meta])* trait $add_cloned_iter_trait:tt;
+        $(#[$add_cloned_iter_fn_doc:meta])* fn $add_cloned_iter_fn:tt;
+
+        $(#[$add_empty_trait_doc:meta])* trait $add_empty_trait:tt;
+        $(#[$add_empty_fn_doc:meta])* fn $add_empty_fn:tt;
+
         $push_trait:tt $push_fn:tt $pushed_alias:tt
     ) => {
+        /// Type alias for the output of applying $push_fn to previous fields.
         pub type $pushed_alias<PrevFields, NewLabel, NewDType> =
             <PrevFields as $push_trait<FieldSpec<NewLabel, NewDType>>>::Output;
 
+        $(#[$add_trait_doc])*
         pub trait $add_trait<NewLabel, NewDType> {
+            /// [FieldCons](../fieldlist/type.FieldCons.html) cons-list after adding field.
             type OutputFields: AssocStorage;
 
+            $(#[$add_fn_doc])*
             fn $add_fn(self, data: FieldData<NewDType>) -> DataStore<Self::OutputFields>;
         }
 
@@ -174,9 +206,12 @@ macro_rules! make_add_field {
             }
         }
 
+        $(#[$add_valiter_trait_doc])*
         pub trait $add_valiter_trait<NewLabel, NewDType> {
+            /// [FieldCons](../fieldlist/type.FieldCons.html) cons-list after adding field.
             type OutputFields: AssocStorage;
 
+            $(#[$add_valiter_fn_doc])*
             fn $add_valiter_fn<IntoIter, Iter>(
                 self,
                 iter: IntoIter,
@@ -218,9 +253,12 @@ macro_rules! make_add_field {
             }
         }
 
+        $(#[$add_iter_trait_doc])*
         pub trait $add_iter_trait<NewLabel, NewDType> {
+            /// [FieldCons](../fieldlist/type.FieldCons.html) cons-list after adding field.
             type OutputFields: AssocStorage;
 
+            $(#[$add_iter_fn_doc])*
             fn $add_iter_fn<IntoIter, Iter>(self, iter: IntoIter) -> DataStore<Self::OutputFields>
             where
                 Iter: Iterator<Item = NewDType>,
@@ -256,9 +294,12 @@ macro_rules! make_add_field {
             }
         }
 
+        $(#[$add_cloned_valiter_trait_doc])*
         pub trait $add_cloned_valiter_trait<NewLabel, NewDType> {
+            /// [FieldCons](../fieldlist/type.FieldCons.html) cons-list after adding field.
             type OutputFields: AssocStorage;
 
+            $(#[$add_cloned_valiter_fn_doc])*
             fn $add_cloned_valiter_fn<'a, IntoIter, Iter>(
                 self,
                 iter: IntoIter,
@@ -304,9 +345,12 @@ macro_rules! make_add_field {
             }
         }
 
+        $(#[$add_cloned_iter_trait_doc])*
         pub trait $add_cloned_iter_trait<NewLabel, NewDType> {
+            /// [FieldCons](../fieldlist/type.FieldCons.html) cons-list after adding field.
             type OutputFields: AssocStorage;
 
+            $(#[$add_cloned_iter_fn_doc])*
             fn $add_cloned_iter_fn<'a, IntoIter, Iter>(
                 self,
                 iter: IntoIter,
@@ -352,9 +396,12 @@ macro_rules! make_add_field {
             }
         }
 
+        $(#[$add_empty_trait_doc])*
         pub trait $add_empty_trait<NewLabel, NewDType> {
+            /// [FieldCons](../fieldlist/type.FieldCons.html) cons-list after adding field.
             type OutputFields: AssocStorage;
 
+            $(#[$add_empty_fn_doc])*
             fn $add_empty_fn(self) -> DataStore<Self::OutputFields>;
         }
         impl<PrevFields, NewLabel, NewDType> $add_empty_trait<NewLabel, NewDType>
@@ -384,6 +431,7 @@ macro_rules! make_add_field {
         where
             PrevFields: AssocStorage,
         {
+            $(#[$add_fn_doc])*
             pub fn $add_fn<NewLabel, NewDType>(
                 self,
                 data: FieldData<NewDType>,
@@ -394,6 +442,7 @@ macro_rules! make_add_field {
                 $add_trait::$add_fn(self, data)
             }
 
+            $(#[$add_valiter_fn_doc])*
             pub fn $add_valiter_fn<NewLabel, NewDType, IntoIter, Iter>(
                 self,
                 iter: IntoIter,
@@ -406,6 +455,7 @@ macro_rules! make_add_field {
                 $add_valiter_trait::$add_valiter_fn(self, iter)
             }
 
+            $(#[$add_iter_fn_doc])*
             pub fn $add_iter_fn<NewLabel, NewDType, IntoIter, Iter>(
                 self,
                 iter: IntoIter,
@@ -418,6 +468,7 @@ macro_rules! make_add_field {
                 $add_iter_trait::$add_iter_fn(self, iter)
             }
 
+            $(#[$add_cloned_valiter_fn_doc])*
             pub fn $add_cloned_valiter_fn<'a, NewLabel, NewDType, IntoIter, Iter>(
                 self,
                 iter: IntoIter,
@@ -430,7 +481,7 @@ macro_rules! make_add_field {
             {
                 $add_cloned_valiter_trait::$add_cloned_valiter_fn(self, iter)
             }
-
+            $(#[$add_cloned_iter_fn_doc])*
             pub fn $add_cloned_iter_fn<'a, NewLabel, NewDType, IntoIter, Iter>(
                 self,
                 iter: IntoIter,
@@ -444,6 +495,7 @@ macro_rules! make_add_field {
                 $add_cloned_iter_trait::$add_cloned_iter_fn(self, iter)
             }
 
+            $(#[$add_empty_fn_doc])*
             pub fn $add_empty_fn<NewLabel, NewDType>(
                 self,
             ) -> DataStore<<Self as $add_empty_trait<NewLabel, NewDType>>::OutputFields>
@@ -457,22 +509,94 @@ macro_rules! make_add_field {
 }
 
 make_add_field![
-    PushFrontField push_front_field;
-    PushFrontFromValueIter push_front_from_value_iter;
-    PushFrontFromIter push_front_from_iter;
-    PushFrontClonedFromValueIter push_front_cloned_from_value_iter;
-    PushFrontClonedFromIter push_front_cloned_from_iter;
-    PushFrontEmpty push_front_empty;
+    /// Trait for pushing a [FieldData](../field/struct.FieldData.html) onto the front of a
+    /// [DataStore](struct.DataStore.html)'s fields cons-list.
+    trait PushFrontField;
+    /// Push a [FieldData](../field/struct.FieldData.html) onto the front of this store's
+    /// fields cons-list.
+    fn push_front_field;
+
+    /// Trait for pushing a field onto the front of a [DataStore](struct.DataStore.html)'s fields
+    /// cons-list using data from an iterator of [Value](../field/enum.Value.html) objects.
+    trait PushFrontFromValueIter;
+    /// Push a field onto the front of this store's fields cons-list using data from an iterator
+    /// of [Value](../field/enum.Value.html) objects.
+    fn push_front_from_value_iter;
+
+    /// Trait for pushing a field onto the front of a [DataStore](struct.DataStore.html)'s fields
+    /// cons-list using data from an iterator of objects. Field is assumed to have no missing data.
+    trait PushFrontFromIter;
+    /// Push a field onto the front of this store's fields cons-list using data from an iterator
+    /// of objects.
+    fn push_front_from_iter;
+
+    /// Trait for pushing a field onto the front of a [DataStore](struct.DataStore.html)'s fields
+    /// cons-list cloning data from an iterator of [Value](../field/enum.Value.html) objects.
+    trait PushFrontClonedFromValueIter;
+    /// Push a field onto the front of this store's fields cons-list cloning data from an iterator
+    /// of [Value](../field/enum.Value.html) objects.
+    fn push_front_cloned_from_value_iter;
+
+    /// Trait for pushing a field onto the front of a [DataStore](struct.DataStore.html)'s fields
+    /// cons-list cloning data from an iterator of objects. Field is assumed to have no missing
+    /// data.
+    trait PushFrontClonedFromIter;
+    /// Push a field onto the front of this store's fields cons-list cloning data from an iterator
+    /// of objects.
+    fn push_front_cloned_from_iter;
+
+    /// Trait for pushing an empty field onto the front of a [DataStore](struct.DataStore.html)'s
+    /// fields cons-list.
+    trait PushFrontEmpty;
+    /// Push an empty field into the front of this store's fields cons-list.
+    fn push_front_empty;
+
     PushFront push_front PushedFrontField
 ];
 
 make_add_field![
-    PushBackField push_back_field;
-    PushBackFromValueIter push_back_from_value_iter;
-    PushBackFromIter push_back_from_iter;
-    PushBackClonedFromValueIter push_back_cloned_from_value_iter;
-    PushBackClonedFromIter push_back_cloned_from_iter;
-    PushBackEmpty push_back_empty;
+    /// Trait for pushing a [FieldData](../field/struct.FieldData.html) onto the back of a
+    /// [DataStore](struct.DataStore.html)'s fields cons-list.
+    trait PushBackField;
+    /// Push a [FieldData](../field/struct.FieldData.html) onto the back of this store's
+    /// fields cons-list.
+    fn push_back_field;
+
+    /// Trait for pushing a field onto the back of a [DataStore](struct.DataStore.html)'s fields
+    /// cons-list using data from an iterator of [Value](../field/enum.Value.html) objects.
+    trait PushBackFromValueIter;
+    /// Push a field onto the back of this store's fields cons-list using data from an iterator
+    /// of [Value](../field/enum.Value.html) objects.
+    fn push_back_from_value_iter;
+
+    /// Trait for pushing a field onto the back of a [DataStore](struct.DataStore.html)'s fields
+    /// cons-list using data from an iterator of objects. Field is assumed to have no missing data.
+    trait PushBackFromIter;
+    /// Push a field onto the back of this store's fields cons-list using data from an iterator
+    /// of objects.
+    fn push_back_from_iter;
+
+    /// Trait for pushing a field onto the back of a [DataStore](struct.DataStore.html)'s fields
+    /// cons-list cloning data from an iterator of [Value](../field/enum.Value.html) objects.
+    trait PushBackClonedFromValueIter;
+    /// Push a field onto the back of this store's fields cons-list cloning data from an iterator
+    /// of [Value](../field/enum.Value.html) objects.
+    fn push_back_cloned_from_value_iter;
+
+    /// Trait for pushing a field onto the back of a [DataStore](struct.DataStore.html)'s fields
+    /// cons-list cloning data from an iterator of objects. Field is assumed to have no missing
+    /// data.
+    trait PushBackClonedFromIter;
+    /// Push a field onto the back of this store's fields cons-list cloning data from an iterator
+    /// of objects.
+    fn push_back_cloned_from_iter;
+
+    /// Trait for pushing an empty field onto the back of a [DataStore](struct.DataStore.html)'s
+    /// fields cons-list.
+    trait PushBackEmpty;
+    /// Push an empty field into the back of this store's fields cons-list.
+    fn push_back_empty;
+
     PushBack push_back PushedBackField
 ];
 
@@ -493,7 +617,9 @@ where
 }
 impl<Fields> FieldSelect for DataStore<Fields> where Fields: AssocStorage {}
 
+/// Trait to determine the [FrameLookupCons](../view/type.FrameLookupCons.html) for a field list.
 pub trait AssocFrameLookup {
+    /// The associated `FrameLookupCons`.
     type Output;
 }
 impl AssocFrameLookup for Nil {
@@ -510,6 +636,8 @@ impl<Fields> DataStore<Fields>
 where
     Fields: AssocStorage + AssocFrameLookup,
 {
+    /// Wrap this `DataStore` with a [DataView](../view/struct.DataView.html) object. Utility
+    /// function that leverages [IntoView](trait.IntoView.html).
     pub fn into_view(self) -> <Self as IntoView>::Output
     where
         Self: IntoView,
@@ -517,15 +645,26 @@ where
         IntoView::into_view(self)
     }
 }
+
+/// Trait that provides a method to convert `Self` into a [DataView](../view/struct.DataView.html)
+/// object.
 pub trait IntoView {
-    type Output;
+    /// The `Labels` type parameter for the output `DataView`.
+    type Labels;
+    /// The `Frames` type parameter for the output `DataView`.
+    type Frames;
+    /// The output `DataView` (should always be `DataView<Self::Labels, Self::Frames>`).
+    type Output; // = DataView<Self::Labels, Self::Frames>
+    /// Convert `self` into a [DataView](../view/struct.DataView.html) object.
     fn into_view(self) -> Self::Output;
 }
 impl<Fields> IntoView for DataStore<Fields>
 where
     Fields: AssocStorage + AssocFrameLookup,
 {
-    type Output = DataView<<Fields as AssocFrameLookup>::Output, ViewFrameCons<UTerm, Fields, Nil>>;
+    type Labels = <Fields as AssocFrameLookup>::Output;
+    type Frames = ViewFrameCons<UTerm, Fields, Nil>;
+    type Output = DataView<Self::Labels, Self::Frames>;
 
     fn into_view(self) -> Self::Output {
         DataView::new(ViewFrameCons {
